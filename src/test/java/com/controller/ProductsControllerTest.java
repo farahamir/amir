0cd9cb3aa56c2f941421d30215cee1638ad726ba
hungryleo <amir.hungryleo@gmail.com>
2017-11-17 00:26:54 +0200

    initial project

---
+++ src/test/java/com/controller/ProductsControllerTest.java
@@ -0,0 +1,38 @@
+package com.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductsControllerTest {
+
+    @Test
+    public void testGetProducts() throws Exception {
+
+    }
+
+    @Test
+    public void testGetProduct() throws Exception {
+
+    }
+
+    @Test
+    public void testAddProduct() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateProduct() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveProduct() throws Exception {
+
+    }
+}
