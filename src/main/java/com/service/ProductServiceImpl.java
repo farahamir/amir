d54b9ffca8358a6ef0c5df48f175fb2644140e73
hungryleo <amir.hungryleo@gmail.com>
2017-11-19 03:49:31 +0200

    removed old code

--- src/main/java/com/service/ProductServiceImpl.java
+++ src/main/java/com/service/ProductServiceImpl.java
@@ -1,6 +1,5 @@
 package com.service;
 
-import com.DAO.ProductDAO;
 import com.jpa.CategoryRepository;
 import com.jpa.ProductRepository;
 import com.model.Category;
@@ -8,7 +7,6 @@ import com.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,15 +19,12 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductRepository productRepository;
-    @Autowired
-    private ProductDAO dao;
+
     public int addProduct(Product product) {
-        //return productRepository.saveAndFlush(product).getId();
         productRepository.save(new HashSet<Product>() {{
             add(product);
         }});
         return product.getId();
-        //return dao.addProduct(product);
     }
 
     @Override
@@ -39,8 +34,6 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void updateProduct(int id, Product product) {
-       // int productId = dao.updateProduct(id,product);
-       // product.setId(productId);
         Product found = productRepository.findOne(id);
         found.setName(product.getName());
         found.setCategory(product.getCategory());
@@ -51,13 +44,11 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void deleteProduct(int id) {
         productRepository.delete(id);
-        //dao.deleteProduct(id);
     }
 
     @Override
     public Product findById(int id) {
         return productRepository.findOne(id);
-        //return dao.getProductById(id);
     }
 
     @Override
@@ -68,6 +59,5 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public boolean exists(Product product) {
         return productRepository.exists(product.getId());
-        //return dao.isProductExists(product);
     }
 }
